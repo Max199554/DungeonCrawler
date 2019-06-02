2498eb3672f92870549a485930f90442594f321c
Charlotte <cheby055@mymail.unisa,edu.au>
2019-06-02 13:01:02 +0930

    Map1 with assets

---
+++ Blank LibGDX Project/android/assets/t1_asset/nn.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="nn" tilewidth="16" tileheight="16" tilecount="768" columns="32">
+ <image source="../【懒猫素材】90346-RPG Maker大师像素游戏魔女之家素材/System/TileA1.png" width="512" height="384"/>
+</tileset>
---
+++ Blank LibGDX Project/android/assets/t1_asset/tilee3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="tilee3" tilewidth="16" tileheight="16" tilecount="512" columns="16">
+ <image source="../assets/TileA5.png" width="256" height="512"/>
+</tileset>
