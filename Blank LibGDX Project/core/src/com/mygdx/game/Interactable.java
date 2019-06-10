dcc245233aa0b59e21071c0a5893ab38a76dc1e6
ken <heyty005@unisa.edu.au>
2019-06-10 16:11:05 +0930

    tidy up

--- Blank LibGDX Project/core/src/com/mygdx/game/Interactable.java
+++ Blank LibGDX Project/core/src/com/mygdx/game/Interactable.java
@@ -3,6 +3,7 @@ package com.mygdx.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Rectangle;
@@ -11,17 +12,23 @@ import com.badlogic.gdx.math.Vector2;
 public class Interactable {
     Vector2 position;
     Circle interactCircle;
+    Sprite sprite;
     Texture img = new Texture("LevelChangeDoor.png");
+
     public boolean canSetScene = false;
 
     public Interactable(Vector2 position, float interactRadius){
+        sprite = new Sprite(img);
+
+        sprite.setPosition(position.x, position.y);
+        sprite.setScale(3);
         this.position = position;
         interactCircle = new Circle(position, interactRadius);
         interactCircle.setPosition(position);
     }
 
     public void render(SpriteBatch batch){
-        batch.draw(img, position.x, position.y);
+        sprite.draw(batch);
     }
 
    public void DetactDisToPlayer(float x, float y){
